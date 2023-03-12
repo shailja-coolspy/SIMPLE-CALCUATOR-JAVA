1d6a4fc8bdfeb7835d4be08cda4b80fcd60faecb
Sgcts <sgcts@Gameings-iMac.local>
2023-03-11 20:50:16 -0800

    Added SecondMain

---
+++ SecondMain.java
@@ -0,0 +1,23 @@
+public class SecondMain{
+	public String username;
+	public String password;
+	public Main(){}
+	
+	public void setUsername(String u){
+		this.username = u;
+	}
+
+	public void setPassword(String p){
+		this.password = p;
+	}
+
+	public String getUsername(){
+		return this.username;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+}
+
+~
